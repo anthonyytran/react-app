5cf1aa84e8b3fb9288d544746464a1e69aa12eb9
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2024-01-15 00:51:58 +1100

    Event handlers

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,31 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-    return <ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>
+  let items = [
+    "New York",
+    "Los Angeles",
+    "San Francisco",
+    "Tokyo",
+    "London",
+    "Paris",
+  ];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
